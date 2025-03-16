6fd77a1de0f3baa040395619a97bb57cc8029e48
amruthn1 <amruthnadimpally1@gmail.com>
2025-03-16 18:05:30 -0400

    Update install scripts

--- LLM_for_TMO_materials/install_arm.sh
+++ LLM_for_TMO_materials/install_arm.sh
@@ -1,10 +1,12 @@
 CONDA_SUBDIR=osx-arm64 conda env create -f env.yml
 conda activate crystal-llm
 
-pip install torch torchvision torchaudio
+pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0
+pip install dgl -f https://data.dgl.ai/wheels/repo.html
 pip install accelerate peft transformers sentencepiece datasets
 pip install bitsandbytes pymatgen wandb
-pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+pip install "unsloth @ git+https://github.com/unslothai/unsloth.git"
 pip install --no-deps "trl<0.9.0"
+pip install mp_api ase matgl
 
 conda deactivate
--- LLM_for_TMO_materials/install_cuda.sh
+++ LLM_for_TMO_materials/install_cuda.sh
@@ -4,10 +4,12 @@
 conda env create -f env.yml
 conda activate crystal-llm
 
-pip install torch torchvision torchaudio
+pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu121
+pip install dgl -f https://data.dgl.ai/wheels/torch-2.4/cu121/repo.html
 pip install accelerate peft transformers sentencepiece datasets
 pip install bitsandbytes pymatgen wandb
-pip install "unsloth[cu118-torch240] @ git+https://github.com/unslothai/unsloth.git"
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 pip install --no-deps "trl<0.9.0"
+pip install mp_api ase matgl
 
 conda deactivate
